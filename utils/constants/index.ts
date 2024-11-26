d5bc28dd2e39709f1659ac0760b4b53366753311
Gabriel <gabrielalcantaratp@gmail.com>
2024-11-26 12:20:50 -0300

    feat: CAKTOAPP-19 - add constants that will be used in start screen

---
+++ utils/constants/index.ts
@@ -0,0 +1,6 @@
+import { Url } from "./urls";
+import { Text } from "./texts";
+export const Constants = Object.freeze({
+  Url,
+  Text,
+});
---
+++ utils/constants/urls.ts
@@ -0,0 +1,5 @@
+const register = "https://app.cakto.com.br/auth/register";
+
+export const Url = Object.freeze({
+  register,
+});
