82694a5f568a1aec2ecd868878d274c0a4e9c1d0
Gabriel <gabrielalcantaratp@gmail.com>
2024-11-29 11:27:18 -0300

    feat: CAKTOAPP22 - improve UX of loading and bottom sheet modal

--- components/ui/modal/bottom-sheet.tsx
+++ components/ui/modal/bottom-sheet.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useRef, useState } from "react";
+import { Animated, Modal, View } from "react-native";
 import { Image } from "@/assets/images";
 import { theme } from "@/styles/theme";
-import { Alert, Modal, TouchableOpacity, View } from "react-native";
 import { Text } from "../texts";
 import { Button } from "../buttons";
+import { AnimatedOverlay } from "./animated-overlay";
 
 export interface BottomSheetProps {
   title?: string;
@@ -17,20 +19,13 @@ export interface BottomSheetProps {
 }
 
 export function BottomSheet(props: BottomSheetProps) {
-  const {
-    title,
-    description,
-    scheme = "warning",
-    onSubmit,
-    onSubmitText,
-    onCancel,
-    onCancelText,
-    isVisible = false,
-    onClose,
-  } = props;
+  const { isVisible = false, onClose, ...rest } = props;
+  const [slideAnimation] = useState(new Animated.Value(300));
+  const [showModal, setShowModal] = useState(false);
+  const data = useRef<Partial<BottomSheetProps> | null>(null);
 
   const colorScheme =
-    scheme === "warning"
+    data.current?.scheme === "warning"
       ? { title: theme.color.yellow.alert, button: theme.color.yellow.alert }
       : {
           title: theme.color.secondary.bright,
@@ -38,93 +33,117 @@ export function BottomSheet(props: BottomSheetProps) {
         };
 
   const icon =
-    scheme === "warning" ? (
+    data.current?.scheme === "warning" ? (
       <Image.Warning.Triangle />
     ) : (
       <Image.Success.CheckCircle />
     );
 
+  useEffect(() => {
+    if (isVisible) {
+      setShowModal(true);
+      data.current = { ...rest };
+      Animated.timing(slideAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(slideAnimation, {
+        toValue: 300,
+        duration: 300,
+        useNativeDriver: true,
+      }).start(() => {
+        setShowModal(false);
+        data.current = null;
+      });
+    }
+  }, [isVisible]);
+
+  if (!showModal) return null;
+
   return (
     <Modal
-      visible={isVisible}
+      visible={showModal}
       transparent
       onRequestClose={onClose}
-      animationType="slide"
+      animationType="none"
     >
-      <TouchableOpacity
+      <AnimatedOverlay
+        isVisible={isVisible}
         onPress={onClose}
+        backgroundColor={theme.color.gray["064"]}
+      />
+      <Animated.View
         style={{
-          flex: 1,
-          justifyContent: "flex-end",
-          backgroundColor: theme.color.gray["064"],
+          transform: [{ translateY: slideAnimation }],
+          padding: theme.spacing.xxs,
+          backgroundColor: theme.color.gray[900],
+          width: theme.size.full,
+          alignItems: "center",
+          borderTopLeftRadius: theme.borderRadius.md,
+          borderTopRightRadius: theme.borderRadius.md,
+          position: "absolute",
+          bottom: 0,
         }}
       >
+        {icon}
+        <View
+          style={{ gap: theme.spacing.nano, marginTop: theme.spacing.xxxs }}
+        >
+          {data.current?.title && (
+            <Text.Base
+              style={{
+                color: colorScheme.title,
+                fontSize: theme.font.size.md,
+                textAlign: "center",
+              }}
+            >
+              {data.current.title}
+            </Text.Base>
+          )}
+          {data.current?.description && (
+            <Text.Base
+              style={{
+                color: theme.color.white[100],
+                fontSize: theme.font.size.xxs,
+                lineHeight: 21,
+                fontFamily: theme.font.family.medium,
+                fontWeight: theme.font.weight.medium,
+                textAlign: "center",
+              }}
+            >
+              {data.current.description}
+            </Text.Base>
+          )}
+        </View>
         <View
           style={{
-            padding: theme.spacing.xxs,
-            backgroundColor: theme.color.gray[900],
+            marginTop: theme.spacing.xxs,
+            gap: theme.spacing.xxxs,
             width: theme.size.full,
-            alignItems: "center",
-            borderTopLeftRadius: theme.borderRadius.md,
-            borderTopRightRadius: theme.borderRadius.md,
           }}
         >
-          {icon}
-          <View
-            style={{ gap: theme.spacing.nano, marginTop: theme.spacing.xxxs }}
-          >
-            {title && (
-              <Text.Base
-                style={{
-                  color: colorScheme.title,
-                  fontSize: theme.font.size.md,
-                  textAlign: "center",
-                }}
-              >
-                {title}
-              </Text.Base>
-            )}
-            {description && (
-              <Text.Base
-                style={{
-                  color: theme.color.white[100],
-                  fontSize: theme.font.size.xxs,
-                  lineHeight: 21,
-                  fontFamily: theme.font.family.medium,
-                  fontWeight: theme.font.weight.medium,
-                  textAlign: "center",
-                }}
-              >
-                {description}
-              </Text.Base>
-            )}
-          </View>
-          <View
-            style={{
-              marginTop: theme.spacing.xxs,
-              gap: theme.spacing.xxxs,
-              width: theme.size.full,
-            }}
-          >
-            {typeof onSubmit === "function" && onSubmitText && (
+          {typeof data.current?.onSubmit === "function" &&
+            data.current?.onSubmitText && (
               <Button.Base
-                title={onSubmitText}
+                title={data.current.onSubmitText}
                 textProps={{ color: theme.color.gray[900] }}
                 style={{ backgroundColor: colorScheme.button }}
-                onPress={onSubmit}
+                onPress={data.current?.onSubmit}
               />
             )}
-            {typeof onCancel === "function" && onCancelText && (
+          {typeof data.current?.onCancel === "function" &&
+            data.current?.onCancelText && (
               <Button.Base
                 variant="unfilled"
-                title={onCancelText}
+                title={data.current.onCancelText}
                 textProps={{ color: colorScheme.button }}
-                onPress={onCancel}
+                onPress={data.current.onCancel}
               />
             )}
-          </View>
         </View>
-      </TouchableOpacity>
+      </Animated.View>
     </Modal>
   );
 }
