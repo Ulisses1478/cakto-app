202e04fa1f4d845238de0e1e977aaa84178c06bd
gabriel <gabriel.pedro@ubistart.com>
2025-01-13 15:49:14 -0300

    feat: CAKTOAPP-162 - add custom bottom sheet modal

--- src/components/ui/modal/index.ts
+++ src/components/ui/modal/index.ts
@@ -1,13 +1,27 @@
 import { BottomSheet, BottomSheetProps, BottomSheetRef } from "./bottom-sheet";
+import {
+  CustomBottomSheet,
+  CustomBottomSheetProps,
+  CustomBottomSheetRef,
+} from "./custom-bottom-sheet";
 import { EditInfo, EditInfoProps, EditInfoRef } from "./edit-info";
 import { Loading } from "./loading";
 
-export const Modal = Object.freeze({ Loading, BottomSheet, EditInfo });
+export const Modal = Object.freeze({
+  Loading,
+  BottomSheet,
+  EditInfo,
+  CustomBottomSheet,
+});
 
 export interface ModalProps {
   BottomSheet: {
     props: BottomSheetProps;
     ref: BottomSheetRef;
+  };
+  CustomBottomSheet: {
+    props: CustomBottomSheetProps;
+    ref: CustomBottomSheetRef;
   };
   EditInfo: {
     props: EditInfoProps;
